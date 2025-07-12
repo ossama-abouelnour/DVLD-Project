ca94ca597835404c4809d5d1378cfa906c20a7fe
Ossama A. <ossama_abouelnour@outlook.com>
2025-07-12 17:05:44 +0100

    List International License Form created

---
+++ DVLD-Project/Applications/International License/frmListInternationalLicenseApplications.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace DVLD_Project.Applications.International_License
+{
+    partial class frmListInternationalLicenseApplications
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRecordsCount = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnClose = new MaterialSkin.Controls.MaterialButton();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.cmsApplications = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
+            this.dgvILlDrivingLicenseApplications = new System.Windows.Forms.DataGridView();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNewApplication = new System.Windows.Forms.Button();
+            this.showPersonDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showLicenseDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showPersonLicenseHistoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsApplications.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvILlDrivingLicenseApplications)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 18);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Filter By";
+            // 
+            // lblRecordsCount
+            // 
+            this.lblRecordsCount.AutoSize = true;
+            this.lblRecordsCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCount.Location = new System.Drawing.Point(116, 583);
+            this.lblRecordsCount.Name = "lblRecordsCount";
+            this.lblRecordsCount.Size = new System.Drawing.Size(27, 20);
+            this.lblRecordsCount.TabIndex = 66;
+            this.lblRecordsCount.Text = "??";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 585);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 18);
+            this.label9.TabIndex = 64;
+            this.label9.Text = "#Records:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnClose.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnClose.Depth = 0;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.HighEmphasis = true;
+            this.btnClose.Icon = null;
+            this.btnClose.Location = new System.Drawing.Point(1127, 577);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnClose.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnClose.Name = "btnClose";
+            this.btnClose.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnClose.Size = new System.Drawing.Size(66, 36);
+            this.btnClose.TabIndex = 63;
+            this.btnClose.Text = "Close";
+            this.btnClose.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnClose.UseAccentColor = false;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Lucida Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.lblTitle.Location = new System.Drawing.Point(370, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(472, 23);
+            this.lblTitle.TabIndex = 56;
+            this.lblTitle.Text = "International Driving License Applications";
+            // 
+            // cmsApplications
+            // 
+            this.cmsApplications.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showPersonDetailsToolStripMenuItem,
+            this.showLicenseDetailsToolStripMenuItem,
+            this.showPersonLicenseHistoryToolStripMenuItem});
+            this.cmsApplications.Name = "contextMenuStrip1";
+            this.cmsApplications.Size = new System.Drawing.Size(226, 92);
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Location = new System.Drawing.Point(262, 154);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(186, 20);
+            this.txtFilterValue.TabIndex = 61;
+            // 
+            // cbFilterBy
+            // 
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.Items.AddRange(new object[] {
+            "None",
+            "International License ID",
+            "Application ID",
+            "Driver ID",
+            "Local License ID",
+            "Active"});
+            this.cbFilterBy.Location = new System.Drawing.Point(98, 154);
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(158, 21);
+            this.cbFilterBy.TabIndex = 60;
+            // 
+            // dgvILlDrivingLicenseApplications
+            // 
+            this.dgvILlDrivingLicenseApplications.AllowUserToAddRows = false;
+            this.dgvILlDrivingLicenseApplications.AllowUserToDeleteRows = false;
+            this.dgvILlDrivingLicenseApplications.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvILlDrivingLicenseApplications.ContextMenuStrip = this.cmsApplications;
+            this.dgvILlDrivingLicenseApplications.Location = new System.Drawing.Point(12, 189);
+            this.dgvILlDrivingLicenseApplications.Name = "dgvILlDrivingLicenseApplications";
+            this.dgvILlDrivingLicenseApplications.ReadOnly = true;
+            this.dgvILlDrivingLicenseApplications.Size = new System.Drawing.Size(1178, 375);
+            this.dgvILlDrivingLicenseApplications.TabIndex = 57;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::DVLD_Project.Properties.Resources.circle_xmark;
+            this.pictureBox12.Location = new System.Drawing.Point(1084, 577);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(36, 36);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox12.TabIndex = 65;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::DVLD_Project.Properties.Resources.Applications;
+            this.pictureBox1.Location = new System.Drawing.Point(505, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 58;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNewApplication
+            // 
+            this.btnNewApplication.BackgroundImage = global::DVLD_Project.Properties.Resources.Add_Application;
+            this.btnNewApplication.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnNewApplication.FlatAppearance.BorderSize = 0;
+            this.btnNewApplication.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewApplication.Location = new System.Drawing.Point(1124, 145);
+            this.btnNewApplication.Name = "btnNewApplication";
+            this.btnNewApplication.Size = new System.Drawing.Size(66, 38);
+            this.btnNewApplication.TabIndex = 62;
+            this.btnNewApplication.UseVisualStyleBackColor = true;
+            this.btnNewApplication.Click += new System.EventHandler(this.btnNewApplication_Click);
+            // 
+            // showPersonDetailsToolStripMenuItem
+            // 
+            this.showPersonDetailsToolStripMenuItem.Name = "showPersonDetailsToolStripMenuItem";
+            this.showPersonDetailsToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.showPersonDetailsToolStripMenuItem.Text = "Show Person Details";
+            this.showPersonDetailsToolStripMenuItem.Click += new System.EventHandler(this.showPersonDetailsToolStripMenuItem_Click);
+            // 
+            // showLicenseDetailsToolStripMenuItem
+            // 
+            this.showLicenseDetailsToolStripMenuItem.Name = "showLicenseDetailsToolStripMenuItem";
+            this.showLicenseDetailsToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.showLicenseDetailsToolStripMenuItem.Text = "Show License Details";
+            this.showLicenseDetailsToolStripMenuItem.Click += new System.EventHandler(this.showLicenseDetailsToolStripMenuItem_Click);
+            // 
+            // showPersonLicenseHistoryToolStripMenuItem
+            // 
+            this.showPersonLicenseHistoryToolStripMenuItem.Name = "showPersonLicenseHistoryToolStripMenuItem";
+            this.showPersonLicenseHistoryToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.showPersonLicenseHistoryToolStripMenuItem.Text = "Show Person License History";
+            this.showPersonLicenseHistoryToolStripMenuItem.Click += new System.EventHandler(this.showPersonLicenseHistoryToolStripMenuItem_Click);
+            // 
+            // frmListInternationalLicenseApplications
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 640);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRecordsCount);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnNewApplication);
+            this.Controls.Add(this.txtFilterValue);
+            this.Controls.Add(this.cbFilterBy);
+            this.Controls.Add(this.dgvILlDrivingLicenseApplications);
+            this.Name = "frmListInternationalLicenseApplications";
+            this.Text = "frmListInternationalLicenseApplications";
+            this.Load += new System.EventHandler(this.frmListInternationalLicenseApplications_Load);
+            this.cmsApplications.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvILlDrivingLicenseApplications)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRecordsCount;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Label label9;
+        private MaterialSkin.Controls.MaterialButton btnClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ContextMenuStrip cmsApplications;
+        private System.Windows.Forms.Button btnNewApplication;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ComboBox cbFilterBy;
+        private System.Windows.Forms.DataGridView dgvILlDrivingLicenseApplications;
+        private System.Windows.Forms.ToolStripMenuItem showPersonDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showLicenseDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showPersonLicenseHistoryToolStripMenuItem;
+    }
+}
---
+++ DVLD-Project/Applications/International License/frmListInternationalLicenseApplications.cs
@@ -0,0 +1,94 @@
+﻿using DVLD_Business;
+using DVLD_Project.Licenses;
+using DVLD_Project.People.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_Project.Applications.International_License
+{
+    public partial class frmListInternationalLicenseApplications : Form
+    {
+        private DataTable _dt;
+        
+        public frmListInternationalLicenseApplications()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNewApplication_Click(object sender, EventArgs e)
+        {
+            frmNewInternationalLicenseApplication frm = new frmNewInternationalLicenseApplication();
+            frm.ShowDialog();
+            frmListInternationalLicenseApplications_Load(null, null);
+        }
+
+        private void frmListInternationalLicenseApplications_Load(object sender, EventArgs e)
+        {
+            _dt = clsInternationalLicense.GetAllInternationalLicenses();
+            cbFilterBy.SelectedIndex = 0;
+
+            dgvILlDrivingLicenseApplications.DataSource = _dt;
+            lblRecordsCount.Text =dgvILlDrivingLicenseApplications.Rows.Count.ToString();
+
+            if(dgvILlDrivingLicenseApplications.Rows.Count > 0)
+            {
+                dgvILlDrivingLicenseApplications.Columns[0].HeaderText = "Int.License ID";
+                dgvILlDrivingLicenseApplications.Columns[0].Width = 160;
+
+                dgvILlDrivingLicenseApplications.Columns[1].HeaderText = "Application ID";
+                dgvILlDrivingLicenseApplications.Columns[1].Width = 150;
+
+                dgvILlDrivingLicenseApplications.Columns[2].HeaderText = "Driver ID";
+                dgvILlDrivingLicenseApplications.Columns[2].Width = 130;
+
+                dgvILlDrivingLicenseApplications.Columns[3].HeaderText = "L.License ID";
+                dgvILlDrivingLicenseApplications.Columns[3].Width = 130;
+
+                dgvILlDrivingLicenseApplications.Columns[4].HeaderText = "Issue Date";
+                dgvILlDrivingLicenseApplications.Columns[4].Width = 180;
+
+                dgvILlDrivingLicenseApplications.Columns[5].HeaderText = "Expiration Date";
+                dgvILlDrivingLicenseApplications.Columns[5].Width = 180;
+
+                dgvILlDrivingLicenseApplications.Columns[6].HeaderText = "Active";
+                dgvILlDrivingLicenseApplications.Columns[6].Width = 120;
+            }
+        }
+
+        private void showPersonDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int DriverID = (int)dgvILlDrivingLicenseApplications.CurrentRow.Cells[2].Value;
+            int PersonID = clsDriver.FindByPersonID(DriverID).PersonID;
+            frmShowPersonInfo frm = new frmShowPersonInfo(PersonID);
+            frm.ShowDialog();
+        }
+
+        private void showPersonLicenseHistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int DriverID = (int)dgvILlDrivingLicenseApplications.CurrentRow.Cells[2].Value;
+            int PersonID = clsDriver.FindByPersonID(DriverID).PersonID;
+            frmShowLicenseHistory frm = new frmShowLicenseHistory(PersonID);
+            frm.ShowDialog();
+        }
+
+        private void showLicenseDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int InternationalLicenseID = (int)dgvILlDrivingLicenseApplications.CurrentRow.Cells[0].Value;
+            frmShowLicenseInfo frm = new frmShowLicenseInfo(InternationalLicenseID);
+            frm.ShowDialog();
+
+        }
+    }
+}
